e476b45dd2c7dcb742226be5be0667edda765078
Justin <fan.justin4.1@gmail.com>
2022-11-12 15:28:24 -0800

    add location input compnent

---
+++ Fairway/src/app/location-input/location-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-location-input',
+  templateUrl: './location-input.component.html',
+  styleUrls: ['./location-input.component.css']
+})
+export class LocationInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
