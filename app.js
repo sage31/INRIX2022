4a3ffc1ac99710340608d3f44258035e830df449
Stevie Dean <108839414+sage31@users.noreply.github.com>
2022-11-13 00:02:15 -0800

    updated api

--- app.js
+++ app.js
@@ -12,45 +12,37 @@ next();
 app.set('json spaces', 2);
 
 // to query, call: http://localhost:8000/gettoken
-
 var theToken; 
 async function getToken(){
-    let appId = "acidafmx03";
-    let hashToken = "YWNpZGFmbXgwM3xiUmp2QTRZVnBiYU1CWXBWUmkzOTY5aGJDMkZza2w0VDdwbXVGRVBB";
+    let appId = "g2e38y2raf";
+    let hashToken = "ZzJlMzh5MnJhZnw5Rjl1WTRjSm53NXNWUkF4ek5ibld4VjhDeW5lQlJnOVUxQU54YkEx";
     let url = `https://api.iq.inrix.com/auth/v1/appToken?appId=${appId}&hashToken=${hashToken}`;
-
     var requestOptions = {
         method: 'GET'
     };
-
     //Query INRIX for token
     let response = await fetch(url, requestOptions);
     let json = await response.json();
     let output = json.result.token;
     
-    return output;
-    
+    return output;   
 }
 
-
-
 app.get('/findRoute', async function (req, res) {
-
-    
     //Set up URL to query
-    let lat1 = 37.789740;
-    let long1 = -122.399350;
-    let lat2 = 37.753964;
-    let long2 = -122.453083;
+    let lat1 = 37.74854321248655;
+    let long1 = -122.41211087341308;
+    let lat2 = 37.789581828327925;
+    let long2 = -122.45991534958601;
     let url = `https://api.iq.inrix.com/findRoute?wp_1=${lat1}%2C${long1}&wp_2=${lat2}%2C${long2}&maxAlternates=2&format=json`
     //Set up query method
 
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + theToken);
 
     var requestOptions = {
     method: 'GET',
-    headers: {
-        "Authorization": "Bearer " + theToken
-    },
+    headers: myHeaders,
     redirect: 'follow'
     };
 
@@ -59,51 +51,62 @@ app.get('/findRoute', async function (req, res) {
     let json = await response.json();
     let output = json.result;
 
-    //Return token
-
-    let route1 = output.trip.routes[0];
-    let route2 = output.trip.routes[1];
-    let route3 = output.trip.routes[2];
-
     res.json({
-        route1: output.trip.routes[0],
-        route2: output.trip.routes[1],
-        route3: output.trip.routes[2],
+        route1: await getRouteGeography(output.trip.routes[0]),
+        route2: await getRouteGeography(output.trip.routes[1]),
+        route3: await getRouteGeography(output.trip.routes[2]),
     });
-
-    getRouteGeography(route1,route2,route3);
-    //RETURN (using res) :  a json object that contains three routes, with their mileage, time, price, and danger multiplier
-    
 })
 
-function getRouteGeography(route1, route2, route3){
+async function getRouteGeography(route){
 
-    let routes = [route1.id,route2.id,route3.id];
-    routes.forEach(async function (routeID) {
-        console.log(routeID);
-        let url = `https://api.iq.inrix.com/route?routeId=${routeID}&routeOutputFields=all&incidentOutputFields=all&format=json`
-        //let url = `https://api.iq.inrix.com/route?routeId=20116607&routeOutputFields=all&incidentOutputFields=all&format=json`
+    let routeID = route.id;
+    let url = `https://api.iq.inrix.com/route?routeId=${routeID}&routeOutputFields=all&incidentOutputFields=all&format=json`
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + theToken);
 
-        var requestOptions = {
-        method: 'GET',
-        headers: {
-            "Authorization": "Bearer " + theToken
-        },
-        redirect: 'follow'
-        };
+    var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+    };
 
-        let response = await fetch(url, requestOptions);
-        let json = await response.json();
-        let output = await json.result;
-        console.log(output);
-    })
+    var coordSet;
+    var incidentSet;
+    let response = await fetch(url, requestOptions);
+
+    data = await response.json();
+    
+    coordSet = data.result.trip.routes[0].points.coordinates;
+    incidentSet = data.result.incidents;
+    var dangerMultiplier = 1;
+    var mileage = data.result.trip.routes[0].travelTimeMinutes;
+    var time = parseFloat(data.result.trip.routes[0].totalDistance);
+    var basePricePerMile = 2.30; // 30 cents
+    var price = basePricePerMile * mileage * dangerMultiplier;
+    if(incidentSet != undefined){
+        dangerMultiplier = 0;
+        incidentSet.forEach(function(incident) {
+            dangerMultiplier += incident.severity;  
+        })
+        dangerMultiplier /= mileage;
+        if(dangerMultiplier >= 0.5) dangerMultiplier = 0.5;
+        dangerMultiplier += 1;
+    }
+
+    result = {
+        coords : coordSet,
+        multiplier : dangerMultiplier,
+        distance : mileage,
+        tripTime : time,
+        tripPrice : price,
+    }
+    console.log(result);
+    return result;   
 }
 
 //Starting server using listen function
 app.listen(port, async function () {
     console.log("Server has been started at " + port);
     theToken = await getToken();
-    
 })
-
-
