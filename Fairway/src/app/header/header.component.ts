3f72395af2fe9f40e489f7f782e383dfd025bdd2
Justin <fan.justin4.1@gmail.com>
2022-11-13 08:31:37 -0800

    css changes

--- Fairway/src/app/header/header.component.ts
+++ Fairway/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +10,11 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @ViewChild('searchBar') searchBar: ElementRef;
+  openNav() {
+    this.searchBar.nativeElement.style.display = "block";
   }
 
 }
