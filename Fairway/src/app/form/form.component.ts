661098cfc43bee882a53b6404b382d813130e914
Justin <fan.justin4.1@gmail.com>
2022-11-13 05:18:47 -0800

    form button update

--- Fairway/src/app/form/form.component.ts
+++ Fairway/src/app/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import {UsersService} from '../users.service';
 
 
@@ -24,6 +24,19 @@ export class FormComponent implements OnInit {
     this.user.getData2("55 Stevenson Street, San Francisco, CA", "368 Bush Street, San Francisco, CA").subscribe((data: any)=> {
       console.log(data);
     })
+  }
+
+  @ViewChild('searchBar') searchBar: ElementRef;
+  @ViewChild('temp') temp: ElementRef;
+
+  openMenu() {
+    this.searchBar.nativeElement.style.display = "block";
+    this.temp.nativeElement.style.display = "none";
+  }
+  
+  closeMenu() {
+    this.searchBar.nativeElement.style.display = "none";
+    this.temp.nativeElement.style.display = "block";
   }
 
 }
