e6dd7bb37f8f50461527054e458a932181162411
Justin <fan.justin4.1@gmail.com>
2022-11-13 04:18:50 -0800

    set up form component and delete location input

--- Fairway/src/app/form/form.component.ts
+++ Fairway/src/app/form/form.component.ts
@@ -10,18 +10,7 @@ export class FormComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    //function that runs when form is submitted
-    /*function submitForm(event) {
-      event.preventDefault();
 
-     
-    }
-    */
-
-    //assign form to a form variable, start and destination
-    const form = document.getElementById('form');
-
-    //form?.addEventListener('submit', submitForm);
   }
 
 }
